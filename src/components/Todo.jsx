2d78b6a3828bd8ea8558fab77b8f77c8a6aa77bf
sara <csaragonzalez>
2026-03-30 16:38:45 -0500

    Completar tareas

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,25 @@
 function Todo({ todo, setTodos }) {
+  const completar = () => {
+    setTodos((prev) =>
+      prev.map((t) =>
+        t.id === todo.id ? { ...t, completado: !t.completado } : t
+      )
+    );
+  };
+
   const eliminar = () => {
     setTodos((prev) => prev.filter((t) => t.id !== todo.id));
   };
 
   return (
     <div>
-      {todo.texto}
+      <span style={{
+        textDecoration: todo.completado ? "line-through" : "none"
+      }}>
+        {todo.texto}
+      </span>
+
+      <button onClick={completar}>✔</button>
       <button onClick={eliminar}>🗑</button>
     </div>
   );
